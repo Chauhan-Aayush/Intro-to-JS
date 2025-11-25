58927cea83efbd65c25e7e1767344c55ee06c36b
Aayush Chauhan <your_email@example.com>
2025-11-25 08:27:32 +0000

    data_types

---
+++ 01_Basics/data_Types.js
@@ -0,0 +1,16 @@
+"use strict"; // Treat all JS code as Newer Version.
+
+// alert("Hello") // we are using nodeJS not browser
+
+let name = "Aayush"
+let age = 23
+let isLoggedIn = false
+
+
+
+// number =  2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined =>
